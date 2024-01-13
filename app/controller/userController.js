9cf3e766bbdbccae8dfd72df590203c5f4586a80
Umair Ali <umairsadaqat@invotyx.co.uk>
2024-01-13 15:37:55 +0500

    Update userController.js

--- app/controller/userController.js
+++ app/controller/userController.js
@@ -3,4 +3,8 @@ export const userRegister = async (req, res) => {
 
   // validation
   if (!name) return res.status(400).json({ message: "Name is required" });
+  if (!email) return res.status(400).json({ message: "Email is required" });
+  if (!password)
+    return res.status(400).json({ message: "Password is required" });
+  if (!address) return res.status(400).json({ message: "Address is required" });
 };
